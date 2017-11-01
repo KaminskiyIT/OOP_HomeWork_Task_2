8fc7dea84b84139bbb7cdf08d1023181abe63645
KaminskiyPetr <32541031+KaminskiyIT@users.noreply.github.com>
2017-11-01 22:45:10 +0200

    Delete MyException2.java

--- MyException2.java
+++
@@ -1,8 +0,0 @@
-package net.ukr.kaminskiy;
-
-public class MyException2 extends Exception {
-    @Override
-    public String getMessage() {
-        return "Different diagonals are not a rectangle! Specify another point!";
-    }
-}
